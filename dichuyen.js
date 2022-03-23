ca23f6811857e01f30f4e4fc6972ef76b0c60929
Le van son <levansoon>
2022-03-23 23:53:13 +0700

    game

--- dichuyen.js
+++ dichuyen.js
@@ -1,9 +1,18 @@
+
+function bd(){
+    move()
+}
+
 let duong = document.getElementById('sando');
 function move() {
-    duong.style.top = parseInt(duong.style.top)+5 +'px';
+    duong.style.top = parseInt(duong.style.top)+1 +'px';
     requestAnimationFrame(move);
     if (parseInt(duong.style.top) >=0) {
         duong.style.top = -100+'px'
     }
+
 }
-move();
+function stop(){
+    stop1()
+}
+
---
+++ ong nuoc.js
@@ -0,0 +1,26 @@
+class PipeNorth {
+
+    x;
+    y;
+    width;
+    height;
+
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    drawPipeNorth() {
+        ctx.drawImage(imgpipeNorth, this.x, this.y, this.width, this.height);
+    }
+
+    moveLeft() {
+        if (score < 10) {
+            this.x -= 2;
+        } else {
+            this.x -= 2.5;
+        }
+    }
+}
